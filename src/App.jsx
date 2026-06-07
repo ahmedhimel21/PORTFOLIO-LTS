734ddf32863c649ec8582c9462389fd63906e658
Ahmed Himel <ahmedhimel000@gmail.com>
2026-06-07 20:54:50 +0600

    navbar implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-function App() {
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
+const App = () => {
   return (
     <>
-      <h1 className="text-5xl text-purple-600 font-bold">Hello, World!</h1>
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar></Navbar>
+            <Hero></Hero>
+          </div>
+        </div>
+        <About></About>
+        <Experience></Experience>
+        <Tech></Tech>
+        <Works></Works>
+        <Feedbacks></Feedbacks>
+        <div className="relative z-0">
+          <Contact></Contact>
+          <StarsCanvas></StarsCanvas>
+        </div>
+      </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
